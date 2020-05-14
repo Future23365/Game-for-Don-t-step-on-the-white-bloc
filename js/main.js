b7f5ef9ca7c1f514b8edff7f4a701877dec9e499
mma <123@qq.com>
2020-05-14 20:06:28 +0800

    基本流程完成

--- js/main.js
+++ js/main.js
@@ -18,25 +18,16 @@ $(function() {
     let div = [];
     for(let i = 0; i < 4; i++) {
       div[i] = $('<div></div>').on('click', function() {
-        clearInterval(startGame);
-        $('.mask').css('display', 'block');
-        startButton.show('fast', function() {
-          startButton.html('Game Over!');
-          score = 0;
-          speed = 10;
-          bottomPosition = 600;
-          ul.css('bottom', bottomPosition + 'px');
-          ul.empty();
-          add();
-        });
+        end();
       })
     }
     let blackDiv = random();
     div[blackDiv] = $('<div style="background-color: black"></div>').on('click', function() {
       $(this).css('background-color', '#ccc');
+      $(this).off('click');
       score++;
       header.html('您的得分：' + score);
-      if(score % 20 == 0) {
+      if(score % 10 == 0) {
         speed = speed + 5;
       }
     });
@@ -44,19 +35,32 @@ $(function() {
     for(let i = 0; i < 4; i++) {
       li.append(div[i]);
     }
-    ul.prepend(li);
+    $('article ul').prepend(li);
   }
 
   function start() {
     let flag = 0;
+    let index = 0;
     startGame = setInterval(function() {
       bottomPosition -= speed;
       flag += 1;
-      if(flag >= (150/speed - 2)) {
+      if(flag >= Math.floor((150/speed - 2))) {
         add();
         flag = -2;
       }
-      ul.css('bottom', bottomPosition + 'px');
+      $('article ul').css('bottom', bottomPosition + 'px');
+      if(bottomPosition < 0 && ((bottomPosition-speed) % -150 > bottomPosition % -150))
+      {
+        
+        for(let i = 0; i < 3; i++) {
+          console.log($('article ul').find('li').eq(5).find('div').eq(i).css('background-color'))
+          if($('article ul').find('li').eq(5).find('div').eq(i).css('background-color') === 'rgb(0, 0, 0)')
+          {
+            end();
+          }
+        }
+        index ++;
+      }
     }, 50)
   }
 
@@ -65,5 +69,20 @@ $(function() {
     return random;
   }
 
+
+  function end() {
+    clearInterval(startGame);
+        $('.mask').css('display', 'block');
+        startButton.show('fast', function() {
+          startButton.html('Game Over!');
+    score = 0;
+    speed = 10;
+    bottomPosition = 600;
+    $('article ul').css('bottom', bottomPosition + 'px');
+    $('article ul').empty();
+    add();
+        });
+    
+  }
 
 })
